7b61b15fa3691d384e270209f7c0b12667dc006e
Kevin <kh925063@gmail.com>
2026-09-16 13:50:49 -0600

    conexion a bd hecha

---
+++ services/almacen.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+UBICACION_ALMACEN = os.getenv("DATABASE_URL")
+
+motor = create_engine(UBICACION_ALMACEN)
+llaves = sessionmaker(autocommit=False, autoflush=False, bind=motor)
+
+class miClaseBase(DeclarativeBase):
+    pass
+
+
+def abrir_puerta_bd():
+    base_datos = llaves()
+    try:
+        yield base_datos
+    finally:
+        base_datos.close()
+

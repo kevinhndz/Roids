f6254053d5cd217cdbda2cb595b0228130a4e783
Kevin <kh925063@gmail.com>
2026-09-17 12:27:37 -0600

    Refactorizando codigo : capa 1 -> conexion a base de datos

--- database/almacen.py
+++ database/almacen.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy import create_engine
 
 load_dotenv()
@@ -10,9 +10,8 @@ UBICACION_ALMACEN = os.getenv("DATABASE_URL")
 motor = create_engine(UBICACION_ALMACEN)
 llaves = sessionmaker(autocommit=False, autoflush=False, bind=motor)
 
-class miClaseBase(DeclarativeBase):
+class miclaseBase(DeclarativeBase):
     pass
-
 
 def abrir_puerta_bd():
     base_datos = llaves()
@@ -20,4 +19,3 @@ def abrir_puerta_bd():
         yield base_datos
     finally:
         base_datos.close()
-
--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers import login  
-from routers import gerentes
-
-app = FastAPI()
-
-
-app.include_router(login.router)
-app.include_router(gerentes.router)
